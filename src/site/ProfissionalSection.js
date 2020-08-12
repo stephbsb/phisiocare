b0ac824f781a1c5d88d0ce28e8250291e5d6d802
stephbsb <stephany0303@gmail.com>
2020-08-11 21:51:17 -0300

    Adição de detalhes da profissional

--- src/site/ProfissionalSection.js
+++ src/site/ProfissionalSection.js
@@ -1,39 +1,43 @@
-import React from "react";
+import React from 'react';
 
-import "./ProfissionalSection.css";
+import './ProfissionalSection.css';
 
-import foto from "../img/perfil.jpg";
+import foto from '../img/perfil.jpg';
 
 const ProfissionalSection = () => {
   return (
     <section
-      className="profissional-section section scrollspy"
-      id="profissional"
+      className='profissional-section section scrollspy'
+      id='profissional'
     >
-      <div className="container">
-        <div className="row">
-          <div className="col s12 m6 l4 offset-l2 center">
-            <img src={foto} className="responsive-img circle " />
+      <div className='container'>
+        <div className='row'>
+          <div className='col s12 m6 l4 offset-l2 center'>
+            <img src={foto} className='responsive-img circle ' alt='Pamella' />
           </div>
-          <div className="col s12 m6 l4">
+          <div className='col s12 m6 l4'>
             <h5>Pâmella Ribeiro Rodrigues</h5>
             <h6>Fisioterapeuta</h6>
-            <h6>Crefito: 12345</h6>
-            <span className="grey-text">
+            <h6>Crefito: 206394-F</h6>
+            <span className='grey-text text-darken-1'>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Veritatis quo sunt enim aut. Esse quas quod voluptas molestiae,
-                aut illo!{" "}
+                Sou Dra. Pâmella Ribeiro Rodrigues, fisioterapeuta formada na
+                Universidade de Brasília, CREFITO 206394-F, com Certificação em
+                Osteopatia (CO) pela Escola de Madri e especializada nos métodos
+                e técnicas a seguir:
               </p>
 
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
-                porro magnam at, corrupti reprehenderit provident ex tenetur
-                consequatur est sint obcaecati, officia odit dicta quam
-                laudantium eveniet, quae corporis aspernatur incidunt laborum
-                similique assumenda atque necessitatibus? Asperiores
-                consequuntur maiores voluptatem.
-              </p>
+              <ul>
+                <li>
+                  Método Pilates, com cinco anos de experiência em Pilates
+                  clínico funcional;
+                </li>
+                <li>RPG - RPG-Reeducação Postural Global;</li>
+                <li>Dry Needling (Agulhamento a seco);</li>
+                <li>Conceito Mulligan;</li>
+                <li>Método McKenzie (Coluna Lombar);</li>
+                <li>Posturologia.</li>
+              </ul>
             </span>
           </div>
         </div>
