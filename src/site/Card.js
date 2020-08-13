da5404f55dc4c88fd8717eb522c13c5740502ab4
stephbsb <stephany0303@gmail.com>
2020-08-13 20:02:41 -0300

    Mudança de alguns estilos e imagens

--- src/site/Card.js
+++ src/site/Card.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
-import "./Card.css";
+import './Card.css';
 
 const Card = (props) => {
   return (
-    <div className="card-panel center-align card z-depth-4">
-      <div className="card-content">
-        <img src={props.img} className="card-icon" />
-        <h5>{props.especialidade}</h5>
-        <a href={props.href} className="btn modal-trigger cyan">
+    <div className='card-panel center-align card z-depth-4'>
+      <div className='card-content'>
+        <img src={props.img} className='card-icon' />
+        <h6 className='grey-text text-darken-2'>{props.especialidade}</h6>
+        <a href={props.href} className='btn modal-trigger cyan'>
           Saiba Mais
         </a>
       </div>
--- src/site/ProfissionalSection.js
+++ src/site/ProfissionalSection.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './ProfissionalSection.css';
 
-import foto from '../img/perfil.jpg';
+import foto from '../img/pamella.jpeg';
 
 const ProfissionalSection = () => {
   return (
