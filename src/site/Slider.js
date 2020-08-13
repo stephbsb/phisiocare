f1402d82ddaa39b00e1840467db1653714c6119d
stephbsb <stephany0303@gmail.com>
2020-08-13 17:05:49 -0300

    Mudança em mensagem no banner

--- src/site/Slider.js
+++ src/site/Slider.js
@@ -1,40 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import "./Slider.css";
+import './Slider.css';
 
-import img1 from "../img/slider/pilates2.jpg";
-import img2 from "../img/slider/osteopatia-entenda-o-que-e-e-que-doencas-trata.jpeg";
-import img3 from "../img/espaco/vista_panoramica.jpg";
+import img1 from '../img/slider/pilates2.jpg';
+import img2 from '../img/slider/osteopatia-entenda-o-que-e-e-que-doencas-trata.jpeg';
+import img3 from '../img/espaco/vista_panoramica.jpg';
 
 class Slider extends Component {
   render() {
     return (
-      <section className="slider-style" id="home">
-        <div className="slider">
-          <ul className="slides">
+      <section className='slider-style' id='home'>
+        <div className='slider'>
+          <ul className='slides'>
             <li>
               <img src={img1} />
-              <div className="caption left-align">
-                <h2>Em Breve</h2>
-                <h5 className="light grey-text text-lighten-3 ">
+              <div className='caption left-align'>
+                <h2>Estamos Funcionando!</h2>
+                <h5 className='light grey-text text-lighten-3 '>
                   Nova clinica de pilates em Águas Claras.
                 </h5>
               </div>
             </li>
             <li>
               <img src={img2} />
-              <div className="caption right-align">
+              <div className='caption right-align'>
                 <h2>Osteopatia</h2>
-                <h5 className="light grey-text text-lighten-3 ">
+                <h5 className='light grey-text text-lighten-3 '>
                   Entre outras técnicas de fisioterapia.
                 </h5>
               </div>
             </li>
             <li>
               <img src={img3} />
-              <div className="caption left-align">
+              <div className='caption left-align'>
                 <h2>Águas Claras</h2>
-                <h5 className="light grey-text text-lighten-3 ">
+                <h5 className='light grey-text text-lighten-3 '>
                   Próximo à estação de metrô Águas Claras.
                 </h5>
               </div>
